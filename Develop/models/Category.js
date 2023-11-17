760b9b4ddf3c5875d2e75a281862c7ea06196afb
dylan horyza <horyza.dylan@gmail.com>
2023-11-17 13:42:40 -0700

    updated api routes

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
@@ -14,15 +14,15 @@ Category.init(
     },
     category_name: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
