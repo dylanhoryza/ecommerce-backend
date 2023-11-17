89e511b45509d4df2583a6ac06756dd826a9e4d3
dylan horyza <horyza.dylan@gmail.com>
2023-11-17 14:03:27 -0700

    updated server file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -19,7 +19,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false,
   },
-  as: "product_tag",
+  // as: "product_tag",
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
@@ -27,7 +27,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false,
   },
-  as: "tag_product",
+  // as: "tag_product",
 });
 
 module.exports = {
