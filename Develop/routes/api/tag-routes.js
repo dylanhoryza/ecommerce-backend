b505f4dd0823e9d470c4242b8dac89b2749791a1
dylan horyza <horyza.dylan@gmail.com>
2023-11-22 10:45:13 -0700

    fixed syntax changes in routes files

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,12 +3,16 @@ const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
+// find all tags
 router.get("/", async (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product }, { model: ProductTag }],
+      include: [
+        {
+          model: Product,
+          through: ProductTag,
+        },
+      ],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -16,12 +20,16 @@ router.get("/", async (req, res) => {
   }
 });
 
+// find a single tag by its `id`
 router.get("/:id", async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }, { model: ProductTag }],
+      include: [
+        {
+          model: Product,
+          through: ProductTag,
+        },
+      ],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -29,8 +37,8 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// create a new tag
 router.post("/", async (req, res) => {
-  // create a new tag
   try {
     const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
@@ -39,8 +47,8 @@ router.post("/", async (req, res) => {
   }
 });
 
+// update a tag's name by its `id` value
 router.put("/:id", async (req, res) => {
-  // update a tag's name by its `id` value
   try {
     const tagData = await Tag.update(req.body, {
       where: {
@@ -57,8 +65,8 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+// delete on tag by its `id` value
 router.delete("/:id", async (req, res) => {
-  // delete on tag by its `id` value
   try {
     const tagData = await Tag.destroy({
       where: {
